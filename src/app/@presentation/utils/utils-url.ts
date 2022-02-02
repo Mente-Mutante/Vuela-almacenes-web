b7f34dfb260d8f9c33048afafe74d7096129654b
Jesus <jesusaiparraguirre@gmail.com>
2022-02-02 14:55:12 -0500

    fix utils

--- src/app/@presentation/utils/utils-url.ts
+++ src/app/@presentation/utils/utils-url.ts
@@ -1,9 +1,5 @@
 export const Utils = {
-<<<<<<< HEAD
      BASE: 'http://167.172.135.171/'
      // BASE: 'http://localhost:8080/'
-=======
      //BASE: 'http://167.172.135.171/'
-     BASE: 'http://localhost:8080/'
->>>>>>> fe2423ac5c9f93abf172f742ae28d1e0fb678288
 }
